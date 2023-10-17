935aa0108424507a30fc7387fbe06974ecd55a45
frehags-pfc <fredrik.hagstrom@getpfc.com>
2023-10-17 21:23:17 +0200

    Fix main

--- app/Main.hs
+++ app/Main.hs
@@ -3,5 +3,34 @@ module Main
   )
 where
 
+import LexerCombinator
+import Parser
+import ParserCombinator
+import System.Environment (getArgs)
+
+runLexer :: String -> IO ()
+runLexer fileContents = case run lexFile $ unlines . lines $ fileContents of
+  Right (out, _) -> putStrLn $ show out
+  Left (es, _) -> error (concat $ show <$> es)
+
+runParser :: String -> IO ()
+runParser fileContents = case run lexFile $ unlines . lines $ fileContents of
+  Right (out, _) -> case run parseProgram out of
+    Right (out', _) -> putStrLn $ show out'
+    Left (es', _) -> error (concat $ show <$> es')
+  Left (es, _) -> error (concat $ show <$> es)
+
 main :: IO ()
-main = undefined
+main = do
+  args <- getArgs
+  case args of
+    [file] -> do
+      fileContents <- readFile file
+      runParser fileContents
+    [file, mode] -> do
+      fileContents <- readFile file
+      case mode of
+        "lex" -> runLexer fileContents
+        "parse" -> runParser fileContents
+        _ -> putStrLn "Invalid mode"
+    _ -> putStrLn "Wrong number of arguments"
